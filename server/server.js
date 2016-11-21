aa0bfd4a668017f17fc54a77c7b720a56f289e7f
ZhuGongpu <zhugongpu@hotmail.com>
2016-11-21 15:02:32 +0800

    Update.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const globalShortcut = electron.globalShortcut
+const globalShortcut = electron.globalShortcut;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 1366, height: 768})
+    mainWindow = new BrowserWindow({width: 1366, height: 768});
 
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.setMenu(null);
 
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
 
     globalShortcut.register('alt+e', function () {
@@ -29,7 +29,7 @@ function createWindow() {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null
+        mainWindow = null;
         app.quit()
     })
 }
@@ -37,7 +37,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -46,7 +46,7 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
@@ -54,7 +54,7 @@ app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
 
 // In this file you can include the rest of your app's specific main process
